3b506c99624e3fe72f31b515c42ddb79d3d38428
Javier Ortiz Celsi <javierortizcelsi@gmail.com>
2026-02-22 23:45:12 -0300

    update init.lua

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,19 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'ecke/mini.nvim',
+    config = function()
+      require('mini.tabline').setup({
+        show_icons = true, -- muestra iconos de archivos
+        tabpage_section = 'right', -- sección de tabs a la derecha
+      })
+
+      -- Keybindings para navegar buffers
+      vim.keymap.set('n', '<Tab>', ':bnext<CR>', { desc = 'Siguiente buffer', silent = true })
+      vim.keymap.set('n', '<S-Tab>', ':bprev<CR>', { desc = 'Buffer anterior', silent = true })
+      vim.keymap.set('n', '<leader>x', ':bd<CR>', { desc = 'Cerrar buffer', silent = true })
+    end,
+  },
+}
